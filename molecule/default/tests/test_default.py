91568e18e3e278b1ff3f7d953563d33044b65ddb
Grigori Py <pygrigori@gmail.com>
2019-05-28 11:50:03 +0300

    molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_pkg_installed(host):
+    package = host.package('nginx')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+def test_service_is_enabled(host):
+    service = host.service('nginx')
+
+    assert service.is_enabled
+
+
+def test_config_exists(host):
+    file = host.file('/etc/nginx/nginx.conf')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+
+
+def test_config_is_valid(host):
+    cmd = host.run('nginx -t')
+
+    assert not cmd.rc
